8cffd64462754d05756640c52b81fe8c2238571a
Ionut Ovidiu Zailic <ionut@zailic.com>
2015-08-23 18:08:42 +0300

    Fixing failing unit-tests if the runing platform is windows

--- test/testInit.spec.js
+++ test/testInit.spec.js
@@ -21,22 +21,22 @@ describe('jspm plugin init', function(){
     });
 
     it('should add config.js to the top of the files array', function(){
-        expect(files[3].pattern).toEqual(basePath + '/custom_config.js');
+        expect(path.normalize(files[3].pattern)).toEqual(path.normalize(basePath + '/custom_config.js'));
         expect(files[3].included).toEqual(true);
     });
 
     it('should add adapter.js to the top of the files array', function(){
-        expect(files[2].pattern).toEqual(basePath + '/src/adapter.js');
+        expect(path.normalize(files[2].pattern)).toEqual(path.normalize(basePath + '/src/adapter.js'));
         expect(files[2].included).toEqual(true);
     });
 
     it('should add systemjs-polyfills to the top of the files array', function(){
-        expect(files[1].pattern).toEqual(basePath + '/custom_packages/system-polyfills.src.js');
+        expect(path.normalize(files[1].pattern)).toEqual(path.normalize(basePath + '/custom_packages/system-polyfills.src.js'));
         expect(files[1].included).toEqual(true);
     });
 
     it('should add systemjs to the top of the files array', function(){
-        expect(files[0].pattern).toEqual(basePath + '/custom_packages/system.src.js');
+        expect(path.normalize(files[0].pattern)).toEqual(path.normalize(basePath + '/custom_packages/system.src.js'));
         expect(files[0].included).toEqual(true);
     });
 
@@ -45,14 +45,14 @@ describe('jspm plugin init', function(){
     });
 
     it('should add files from jspm.serveFiles to the end of the files array as served files', function(){
-        expect(files[files.length - 1].pattern).toEqual(cwd + '/testfile.js');
+        expect(path.normalize(files[files.length - 1].pattern)).toEqual(path.normalize(cwd + '/testfile.js'));
         expect(files[files.length - 1].included).toEqual(false);
         expect(files[files.length - 1].served).toEqual(true);
         expect(files[files.length - 1].watched).toEqual(true);
     });
 
     it('should use the configured jspm_packages path and include it in the files array', function(){
-        expect(files[4].pattern).toEqual(path.resolve(cwd, './custom_packages/**/*'));
+        expect(path.normalize(files[4].pattern)).toEqual(path.resolve(cwd, './custom_packages/**/*'));
         expect(files[4].included).toEqual(false);
         expect(files[4].served).toEqual(true);
         expect(files[4].watched).toEqual(false); 
